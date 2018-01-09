8fc61a707809b796730b0f369b51c064869865b8
lzj <1161889163@qq.com>
2018-01-09 22:32:56 +0800

    基本实体类

--- src/main/java/com/qcc/domain/Account.java
+++ src/main/java/com/qcc/domain/Account.java
@@ -4,22 +4,72 @@ import com.qcc.enums.ROLE;
 
 import javax.persistence.*;
 import javax.persistence.criteria.CriteriaBuilder;
+import java.util.*;
 
-@Entity
-@Table(name = "tb_account")
+
 public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @Column(name = "user_name")
     private String userName;
+
     private String password;
     @Enumerated(value = EnumType.ORDINAL)
     private ROLE role;
-    private String mobile;
-    private Boolean gender;
-    @Lob
-    @Column(columnDefinition="TEXT")
-    private String description;
+    private Date updateTime;
+    private Date createTime;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public ROLE getRole() {
+        return role;
+    }
+
+    public void setRole(ROLE role) {
+        this.role = role;
+    }
+
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
 
 }
--- src/main/java/com/qcc/domain/House.java
+++ src/main/java/com/qcc/domain/House.java
@@ -2,6 +2,9 @@ package com.qcc.domain;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_house")
@@ -10,7 +13,187 @@ public class House {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private BigDecimal price;
+    //房屋描述
     @Lob
     @Column(columnDefinition="TEXT")
     private String description;
+    private Date updateTime;
+    private Date createTime;
+    private String address;
+    //几室几厅
+    private String room;
+    // 是否整租
+    private Boolean hire;
+    // 装修
+    private String decoration;
+    // 付款方式
+    private String payMent;
+    // 房屋面积
+    private Integer area;
+    // 小区周边
+    private String surrounding;
+    // 配置信息
+    private String configInfo;
+    // 每个房间面积
+    private Integer roomArea;
+    // 朝向
+    private String direction;
+    private String status;
+    //特色
+    private String characteristic;
+    // 租客信息
+    private Account account;
+    private Set<Image> set = new HashSet<Image>();
+    private Set<HouseLog> houseLogs = new HashSet<HouseLog>();
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
+
+    public Boolean getHire() {
+        return hire;
+    }
+
+    public void setHire(Boolean hire) {
+        this.hire = hire;
+    }
+
+    public String getDecoration() {
+        return decoration;
+    }
+
+    public void setDecoration(String decoration) {
+        this.decoration = decoration;
+    }
+
+    public String getPayMent() {
+        return payMent;
+    }
+
+    public void setPayMent(String payMent) {
+        this.payMent = payMent;
+    }
+
+    public Integer getArea() {
+        return area;
+    }
+
+    public void setArea(Integer area) {
+        this.area = area;
+    }
+
+    public String getSurrounding() {
+        return surrounding;
+    }
+
+    public void setSurrounding(String surrounding) {
+        this.surrounding = surrounding;
+    }
+
+    public String getConfigInfo() {
+        return configInfo;
+    }
+
+    public void setConfigInfo(String configInfo) {
+        this.configInfo = configInfo;
+    }
+
+    public Integer getRoomArea() {
+        return roomArea;
+    }
+
+    public void setRoomArea(Integer roomArea) {
+        this.roomArea = roomArea;
+    }
+
+    public String getDirection() {
+        return direction;
+    }
+
+    public void setDirection(String direction) {
+        this.direction = direction;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getCharacteristic() {
+        return characteristic;
+    }
+
+    public void setCharacteristic(String characteristic) {
+        this.characteristic = characteristic;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public Set<Image> getSet() {
+        return set;
+    }
+
+    public void setSet(Set<Image> set) {
+        this.set = set;
+    }
 }
