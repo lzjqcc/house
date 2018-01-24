26d752a778fa65aed581a24db4e1da31fd07e2c6
lzj <1161889163@qq.com>
2018-01-24 22:50:32 +0800

    图片上传

--- src/main/java/com/qcc/utils/CommUtils.java
+++ src/main/java/com/qcc/utils/CommUtils.java
@@ -1,6 +1,7 @@
 package com.qcc.utils;
 
 import com.alibaba.druid.support.json.JSONUtils;
+import com.alibaba.druid.util.StringUtils;
 import com.qcc.domain.Account;
 import com.qcc.domain.BaseEntity;
 import com.qcc.security.AccountToken;
@@ -9,13 +10,15 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import java.io.Closeable;
+import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class CommUtils {
-
+    public static final String IMAGE_DIR = "file:src/main/resources/images";
+    public static final String HOST = "http://localhost:8080/";
     public static <T> ResponseVO<T> buildReponseVo(Boolean success, String message , T t) {
         ResponseVO<T> responseVO = new ResponseVO();
         responseVO.setSuccess(success);
@@ -24,6 +27,13 @@ public class CommUtils {
                responseVO.setResult(t);
         }
         return responseVO;
+    }
+    public static String getImageURL(String path) {
+        if (StringUtils.isEmpty(path)) {
+            return "";
+        }
+        String [] splits = path.split("\\/");
+        return HOST + splits[splits.length - 1];
     }
     public static Account getCurrentAccount() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
